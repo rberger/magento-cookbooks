db119aa7cbf6e452ffe8e80c2d3635834a5c1137
Toni Grigoriu <toni@grigoriu.ro>
2011-03-08 23:58:50 +0100

    fix nfs chef recipe

--- nfs/recipes/server.rb
+++ nfs/recipes/server.rb
@@ -1,9 +1,18 @@
-
 package "nfs-kernel-server"
+
+service "nfs-kernel-server" do
+  service_name "nfs-kernel-server"
+  restart_command "/usr/sbin/invoke-rc.d nfs-kernel-server restart && sleep 1"
+  supports value_for_platform(
+    "default" => { "default" => [:restart] }
+  )
+  action :enable
+end
 
 template "/etc/exports" do
   source "exports.erb"
   owner "root"
   group "root"
   mode 0644
+  notifies :restart, "service[nfs-kernel-server]"
 end
