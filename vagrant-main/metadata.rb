dd9f0451d8a19f810b1116d60e3e329a49c1367d
Toni Grigoriu <toni@grigoriu.ro>
2011-03-09 17:22:12 +0100

    rename ubuntu-common to vagrant-main

--- vagrant-main/metadata.rb
+++ vagrant-main/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email  "toni@grigoriu.ro"
 license           "Apache 2.0"
 description       "Install variuos tools, e.g. ack-grep, htop etc"
 version           "1.0.0"
-recipe            "ubuntu_common", "Install various Ubuntu tools"
+recipe            "vagrant-main", "Install various Ubuntu tools"
 
 %w{ ubuntu debian }.each do |os|
   supports os
